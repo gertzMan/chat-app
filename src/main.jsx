80fd97d1f126610a472b9e29175695fdf4fee9cb
gertzMan <101058145+gertzMan@users.noreply.github.com>
2024-03-26 08:00:09 +0200

    ably connection settings update

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ import './style/main.scss'
 
 
 const client = new Ably.Realtime({ key: import.meta.env.VITE_ABLY_API_KEY, clientId: 'me' });
-// const client = new Ably.Realtime({ key: 'En0fJw.m2gwhA:P7-1RPRu-a5-R2p8CopN37mvdQxOAVitfynfQJtXf8w', clientId: 'me' });
-
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
